69f440fa226cdc16dec48c1b1f95eff727791c12
Pawel Grochola <grocholapawel@gmail.com>
2017-12-03 22:37:50 +0100

    Implement mostly empty view classes

---
+++ aces-and-kings/vis/src/main/java/pl/edu/agh/to2/acesandkings/vis/view/gamescreen/UndoButtonView.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.to2.acesandkings.vis.view.gamescreen;
+
+import pl.edu.agh.to2.acesandkings.vis.view.ButtonView;
+
+/**
+ * Created by Paweł Grochola on 03.12.2017.
+ */
+public class UndoButtonView extends ButtonView {
+}
---
+++ aces-and-kings/vis/src/main/java/pl/edu/agh/to2/acesandkings/vis/view/gamescreen/cards/CardStackView.java
@@ -0,0 +1,11 @@
+package pl.edu.agh.to2.acesandkings.vis.view.gamescreen.cards;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Paweł Grochola on 03.12.2017.
+ */
+public class CardStackView {
+    protected final List<CardView> cardList = new ArrayList<>();
+}
