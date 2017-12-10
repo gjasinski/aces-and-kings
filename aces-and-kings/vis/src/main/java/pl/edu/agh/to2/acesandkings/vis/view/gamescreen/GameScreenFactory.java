aeabe0c67dc8bc893b905ed979364a4cb358d319
Pawel Grochola <grocholapawel@gmail.com>
2017-12-10 20:13:13 +0100

    Fix old model classes still referenced in view

--- aces-and-kings/vis/src/main/java/pl/edu/agh/to2/acesandkings/vis/view/gamescreen/GameScreenFactory.java
+++ aces-and-kings/vis/src/main/java/pl/edu/agh/to2/acesandkings/vis/view/gamescreen/GameScreenFactory.java
@@ -1,6 +1,6 @@
 package pl.edu.agh.to2.acesandkings.vis.view.gamescreen;
 
-import pl.edu.agh.to2.acesandkings.game.model.CardStackDTO;
+import pl.edu.agh.to2.acesandkings.common.model.CardStackObservable;
 
 import java.util.List;
 
@@ -8,7 +8,7 @@ import java.util.List;
  * Created by Paweł Grochola on 03.12.2017.
  */
 public class GameScreenFactory {
-    public GameScreenView createGameScreen(final List<CardStackDTO> cardStackDTOS) {
+    public GameScreenView createGameScreen(final List<CardStackObservable> cardStackObservables) {
         return new GameScreenView();
     }
 }
