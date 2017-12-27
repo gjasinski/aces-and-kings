c90e9e3a2771217eb3c0cd53a8873c6d3c1e0746
UnLow <adam.jamka.1995@gmail.com>
2017-12-27 18:38:40 +0100

    GAME - Added ActiveCardsManipulatorImpl

---
+++ aces-and-kings/game/src/main/java/pl/edu/agh/to2/acesandkings/game/apiImpl/ActiveCardsManipulatorImpl.java
@@ -0,0 +1,27 @@
+package pl.edu.agh.to2.acesandkings.game.apiImpl;
+
+import pl.edu.agh.to2.acesandkings.common.model.Card;
+import pl.edu.agh.to2.acesandkings.common.model.StackPosition;
+import pl.edu.agh.to2.acesandkings.game.api.ActiveCardsManipulator;
+import pl.edu.agh.to2.acesandkings.game.model.CardStackRepositoryImpl;
+
+import java.util.Optional;
+
+public class ActiveCardsManipulatorImpl implements ActiveCardsManipulator {
+    private CardStackRepositoryImpl cardStackRepository;
+
+    @Override
+    public void moveActiveCardToStack(StackPosition sourceStackPosition, StackPosition destinationStackPosition) {
+        moveCard(sourceStackPosition, destinationStackPosition);
+    }
+
+    @Override
+    public void moveCardFromOneBorderStackToOther(StackPosition sourceStackPosition, StackPosition destinationStackPosition) {
+        moveCard(sourceStackPosition, destinationStackPosition);
+    }
+
+    private void moveCard(StackPosition sourceStackPosition, StackPosition destinationStackPosition) {
+        Optional<Card> card = cardStackRepository.removeCardFromStack(sourceStackPosition);
+        card.ifPresent(card1 -> cardStackRepository.putCardOnStack(destinationStackPosition, card1));
+    }
+}
